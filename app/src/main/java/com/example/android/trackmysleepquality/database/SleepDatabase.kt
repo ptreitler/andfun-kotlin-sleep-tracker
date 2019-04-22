62f2ee3406adedd5554308478a5ee92ce53e72e2
Peter Treitler <peter.treitler@gmail.com>
2019-04-22 22:13:21 +0200

    Add SleepDatabase

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -16,16 +16,29 @@
 
 package com.example.android.trackmysleepquality.database
 
-// TODO (01) Create an abstract class that extends RoomDatabase.
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
-// TODO (02) Declare an abstract value of type SleepDatabaseDao.
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+    abstract val sleepDatabaseDao: SleepDatabaseDao
 
-// TODO (03) Declare a companion object.
+    companion object {
+        @Volatile
+        var INSTANCE: SleepDatabase? = null
 
-// TODO (04) Declare a @Volatile INSTANCE variable.
-
-// TODO (05) Define a getInstance() method with a synchronized block.
-
-// TODO (06) Inside the synchronized block:
-// Check whether the database already exists,
-// and if it does not, use Room.databaseBuilder to create it.
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                return INSTANCE ?: Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                ).fallbackToDestructiveMigration().build().also {
+                    INSTANCE = it
+                }
+            }
+        }
+    }
+}
